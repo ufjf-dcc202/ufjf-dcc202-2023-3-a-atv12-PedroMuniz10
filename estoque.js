32e5633cc6cfb857daefe95097c4e809252bebcd
PedroMuniz10 <pedro.lau@estudante.ufjf.br>
2023-12-06 21:00:23 -0300

    fazendo em sala

---
+++ estoque.js
@@ -0,0 +1,20 @@
+
+let estoque = {
+
+    joao: [
+        { tipo: "maca", qtd: 1 },
+        { tipo: "pera", qtd: 1 },
+      ],
+      
+    maria: [
+        { tipo: "maca", qtd: 2 },
+        { tipo: "banana", qtd: 1 },
+      ],
+};
+
+export function getEstoque() {
+    return structuredClone(estoque);
+}
+
+
+//export {getEstoque}
---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+document.entrada.addEventListener('submit', leFormulario);
+
+const olJoao = document.querySelector("#olJoao");
+const olMaria = document.querySelector("#olMaria");
+
+function leFormulario(event) {
+    event.preventDefault();
+    const fruta = document.entrada.fruta.value;
+    const quantidade = document.entrada.quantidade.value;
+
+    console.log(`fruta: ${fruta}, qtd: ${quantidade}`);
+
+    //document.entrada.submit();
+}
