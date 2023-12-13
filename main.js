658651840ee67d232e23e8b9cb9028ce7cc23223
PedroMuniz10 <pedro.lau@estudante.ufjf.br>
2023-12-13 01:09:10 -0300

    Resolvendo o código

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
+import { getEstoque, transacaoNoEstoque, limpaEstoque } from "./estoque.js";
 
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
 
-document.entrada.addEventListener('submit', leFormulario);
+document.entrada.addEventListener('submit', leFormulario)
+document.entrada.reset();
 
-const olJoao = document.querySelector("#olJoao");
-const olMaria = document.querySelector("#olMaria");
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buttonLimparLista').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
 
-function leFormulario(event) {
-    event.preventDefault();
+atualizaTela();
+
+function leFormulario(event){
+    event.preventDefault(event);
+    const quantidade = document.entrada.quantidade.valueAsNumber;
     const fruta = document.entrada.fruta.value;
-    const quantidade = document.entrada.quantidade.value;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
 
-    console.log(`fruta: ${fruta}, qtd: ${quantidade}`);
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 
-    //document.entrada.submit();
+    transacaoNoEstoque(origem, destino, fruta, quantidade);
+    atualizaTela();
+}
+
+function preencheLista(lista, estoqueDaPessoa){
+    lista.textContent = "";
+    if (Array.isArray(estoqueDaPessoa)) {
+        for (let i = 0; i < estoqueDaPessoa.length; i++) {
+            const monte = estoqueDaPessoa[i];
+            const li = document.createElement('li');
+            li.textContent = `${monte.tipo}: ${monte.quantidade}`;
+            lista.appendChild(li);
+        }
+    }
+}
+
+function atualizaTela(){
+    const estoque = getEstoque();
+    
+    olJoao.innerHTML = "";
+    olMaria.innerHTML = "";
+    document.entrada.quantidade.value = 1;
+    document.entrada.fruta.value = "maca";
+
+    preencheLista(olJoao, estoque.joao);
+    preencheLista(olMaria, estoque.maria);
+    
 }
